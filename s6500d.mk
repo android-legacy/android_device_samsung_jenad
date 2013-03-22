e51ebc0f0762d5b5f2d0eb7367360fae3a7463cc
TheWhisp <daavis.90@gmail.com>
2013-03-22 10:05:48 +0200

    initial commit

---
+++ s6500d.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/s6500d/overlay
+
+include device/samsung/msm7x27a-common/msm7x27a.mk
+
+## splash screen
+PRODUCT_COPY_FILES += \
+    device/samsung/s6500d/rootdir/GT-S6500.rle:root/GT-S6500.rle
+   
+$(call inherit-product, vendor/samsung/jena/blobs.mk)
